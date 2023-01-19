bc44fdf2dbbdd5ebc0697975fd9513983a991631
jolak5 <kayodej4@gmail.com>
2023-01-19 10:38:12 +0100

    not changes

---
+++ src/arr.js
@@ -0,0 +1,11 @@
+let arr = [];
+const addButton = document.querySelector('#1')
+
+const addData = () => {
+ arr.push()
+};
+
+addButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    addData();
+  });
