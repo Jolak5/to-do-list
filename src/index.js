4ad354ac228cef5565de285f3f9e15a02df48a7f
jolak5 <kayodej4@gmail.com>
2023-01-26 08:41:53 +0100

    Implement the to do list functions

--- src/index.js
+++ src/index.js
@@ -4,31 +4,49 @@
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
+import Task from './modules/crud.js';
 
-const arr = [
-  {
-    description: 'was the dishes',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'complete To Do list project',
-    completed: false,
-    index: 2,
-  },
-];
+const objTask = new Task();
+const inTask = {};
 
-const list = document.querySelector('ul');
-function createList() {
-  list.insertAdjacentHTML(
-    'afterbegin',
-    ' <hr > <div class="single-list"><div class="li-div"> <input type="checkbox" name="check" value="checkbox"> <li class="list-item"> </div><img src="/dots.svg" alt=""><i class="fa-solid fa-ellipsis-vertical fa-2x"></i></div>',
-  );
+if (localStorage.savedTasks) {
+  objTask.tasks = JSON.parse(localStorage.getItem('savedTasks'));
 }
 
-for (let i = 1; i >= 0; i--) {
-  createList();
-  const listItem = document.querySelector('.list-item');
+const all = document.querySelector('.all');
+const ulElement = document.querySelector('.ulElement');
+const inputElement = document.querySelector('.inputElement');
+const btnCom = document.createElement('button');
+btnCom.innerHTML = 'Clear all completed';
+btnCom.classList.add('btnCom');
 
-  listItem.textContent = arr[i].description;
-}
+inputElement.addEventListener('change', () => {
+  inTask.description = inputElement.value;
+  inTask.completed = false;
+  inTask.index = objTask.tasks.length;
+  objTask.addTask(new Task(inTask.description, inTask.completed, inTask.index));
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    inputElement.value = '';
+  }
+});
+
+btnCom.addEventListener('click', () => {
+  const result = objTask.tasks.filter((task) => task.completed === false);
+  objTask.tasks = result;
+  objTask.populateField();
+  ulElement.innerHTML = '';
+  all.innerHTML = `
+  <div class="title-div title">
+        <p>Today's To Do</p>
+        
+      </div>
+   
+  `;
+  all.append(objTask.showTasks(), btnCom);
+});
+
+objTask.showTasks();
+all.append(btnCom);
